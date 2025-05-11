afc62b25c3fc91a8e9226f354cde7258bf752d70
Haebom <taylor.ahn@hanmail.net>
2025-05-11 14:33:08 +0900

    Fix: Update page.tsx to use actual components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,26 @@
+'use client';
+
 // src/app/page.tsx
 
-// We will import actual components later
-// For now, these are placeholders or will be directly implemented here initially
-// import HSSearchSidebar from "../components/HSSearchSidebar";
-// import TariffVisualization from "../components/TariffVisualization";
-// import NewsFeedSidebar from "../components/NewsFeedSidebar";
+import HSSearchSidebar from "@/components/HSSearchSidebar";
+import TariffDiagram from "@/components/TariffDiagram";
+import NewsFeed from "@/components/NewsFeed";
 
 export default function HomePage() {
   return (
     <main className="page-container"> {/* Corresponds to #container in CSS */}
-      <aside className="sidebar" id="sidebar-left"> {/* Corresponds to #sidebar-left */}
-        <h2>HS Sections + Search</h2>
-        <input type="text" id="hs-search" placeholder="Search HS Code or Description..." />
-        <div className="hs-table-container"> {/* Corresponds to #hs-table-container */}
-          <table className="hs-table"> {/* Corresponds to #hs-table */}
-            <thead>
-              <tr id="hs-table-header"></tr>
-            </thead>
-            <tbody id="hs-table-body"></tbody>
-          </table>
-        </div>
-        <canvas id="hs-chart" className="hs-chart" width="200" height="50"></canvas> {/* Corresponds to #hs-chart */}
-      </aside>
-
+      <HSSearchSidebar />
+      
       <section className="main-content-area"> {/* Corresponds to #main-content */}
         <h2>Hardware Products Imported To The United States From</h2>
         <div id="tariff-visualization" className="tariff-visualization-container">
-          {/* SVG will be injected here by a component later */}
-          <p style={{textAlign: 'center', marginTop: '20px'}}>Tariff Visualization will load here.</p>
+          <TariffDiagram />
         </div>
       </section>
 
       <aside className="sidebar" id="sidebar-right"> {/* Corresponds to #sidebar-right */}
         <h2>Latest News (Tariff)</h2>
-        <ul id="news-list" className="news-list"></ul> {/* Corresponds to #news-list */}
+        <NewsFeed />
       </aside>
     </main>
   );
